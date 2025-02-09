8cf8ddc2461cc4fe580c27df327eeeae05c3d943
Pratik667 <scientisrpsm2222@gmail.com>
2025-02-09 15:47:23 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import Calculator from './learn-redux/Calculator';
+import Layout from './components/layout';
 
 function App() {
   console.log(`App component..`);
   return (    
       <div className="App">
-        <Calculator />
+        <Layout />
       </div>
   );
 }
